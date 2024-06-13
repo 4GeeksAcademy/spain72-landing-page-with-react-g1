f5cdda20c72ff606a617b2493897d40dce944c47
Francisco Javier Carrillo Carrillo <133151654+FcoJCarrillo@users.noreply.github.com>
2024-06-13 19:19:09 +0000

    feat:card

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,27 +2,21 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import pug from '../../img/pug.jpg';
+import { Card } from "./Card";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-			<Card title={"React"} description={"React es una libreria de Javascript orientada para el FrontEnd"}
-			textButton={"Leer más"} imagen={rigoImage}/>
-			<Card title={"React2"} description={"React es una libreria de Javascript orientada para el FrontEnd2"}
-			textButton={"Leer más2"} imagen={rigoImage}/>
+			<div className="container">
+				<div className="row row-cols-1 row-cols-md-3 row-cols-xl-5 g-2 p-3 m-3">
+					<Card/>
+					<Card title={"React"} description={"React es una libreria de Javascript orientada para el FrontEnd"}textButton={"Leer más"} imagen={rigoImage}/>
+					<Card title={"El perro más bonito"} description={"Mira que perro más bonito"} textButton={"Leer más2"} imagen={pug}/>
+					<Card title={"Hola, ¿Qué tal?"} description={"Bienvenido a nuestra página web"}/>
+				</div>
+			</div>
 		</div>
 	);
 };
