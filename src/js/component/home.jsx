d03c7df33c8aa5e52680b37f46fe194e5a561d74
Alimilan88 <162614275+Alimilan88@users.noreply.github.com>
2024-06-13 20:04:14 +0000

    develop home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,18 +6,30 @@ import pug from '../../img/pug.jpg';
 //componentes
 import { Navbar } from "./Navbar.jsx";
 import { Card } from "./Card";
+import { Jumbotron } from "./Jumbotron";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-    <Navbar/>
-			<div className="container">
-				<div className="row row-cols-1 row-cols-md-3 row-cols-xl-5 g-2 p-3 m-3">
-					<Card/>
-					<Card title={"React"} description={"React es una libreria de Javascript orientada para el FrontEnd"}textButton={"Leer más"} imagen={rigoImage}/>
-					<Card title={"El perro más bonito"} description={"Mira que perro más bonito"} textButton={"Leer más2"} imagen={pug}/>
-					<Card title={"Hola, ¿Qué tal?"} description={"Bienvenido a nuestra página web"}/>
+		<div className="justify-content-center">
+			<Navbar />
+			<div>
+				<div className="row my-3 p-2 justify-content-center">
+					<Jumbotron />
+				</div>
+				<div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 p-3 text-center justify-content-center">
+					<div>
+						<Card />
+					</div>
+					<div>
+						<Card title={"React"} description={"React es una libreria de Javascript orientada para el FrontEnd"} textButton={"Leer más"} imagen={rigoImage} />
+					</div>
+					<div>
+						<Card title={"El perro más bonito"} description={"Mira que perro más bonito"} textButton={"Leer más2"} imagen={pug} />
+					</div>
+					<div>
+						<Card title={"Hola, ¿Qué tal?"} description={"Bienvenido a nuestra página web"} />
+					</div>
 				</div>
 			</div>
 		</div>
